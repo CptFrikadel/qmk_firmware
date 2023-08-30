7f192b430b753d2a5769f4333bee33eff7f10d73
Cpt. Frikadel <adkupper@gmail.com>
2023-08-30 22:19:40 +0200

    Keymap: map delete and nav keys

--- keyboards/splitkb/aurora/lily58/keymaps/cpt_frikadel_c/keymap.c
+++ keyboards/splitkb/aurora/lily58/keymaps/cpt_frikadel_c/keymap.c
@@ -30,27 +30,27 @@ void keyboard_pre_init_user(void){
          KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_LBRC,    KC_RBRC, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RCTL,
                         KC_LCTL, KC_LGUI, MO(1), KC_ENT,    KC_SPC, MO(2), KC_BSPC, KC_RALT),
 
-
 [_LAYER1] = LAYOUT(
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-        KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6,                               KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12,
-        KC_GRV, KC_EXLM, KC_AT, KC_HASH, KC_DLR, KC_PERC,                       KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_TILD,
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,      KC_TRNS, KC_NO, KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE,
-                        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                 KC_TRNS, MO(3), KC_TRNS, KC_TRNS),
+        _______, _______, _______, _______, _______, _______,                        _______, _______, _______, _______, _______, KC_DEL,
+        KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                          KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,
+        KC_GRV,  KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                        KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_TILD,
+        _______, _______, _______, _______, _______, _______, _______,      _______, KC_NO, KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE,
+                                   _______, _______, _______, _______,      _______, MO(3), _______, _______),
 
 
 [_LAYER2] = LAYOUT(
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-        KC_GRV, KC_1, KC_2, KC_3, KC_4, KC_5,                                   KC_6, KC_7, KC_8, KC_9, KC_0, KC_TRNS,
-        KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6,                               KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, KC_NO, KC_NO,
-        KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, KC_TRNS,               KC_TRNS, KC_PLUS, KC_MINS, KC_EQL, KC_LBRC, KC_RBRC, KC_BSLS,
-                        KC_TRNS, KC_TRNS, MO(3), KC_TRNS,                   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS),
+        _______, _______, _______, _______, _______, _______,                        _______, _______, _______, _______, _______, _______,
+        KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                           KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______,
+        KC_CAPS, KC_F1,   KC_F3,   KC_F4,   KC_PGUP, KC_HOME,                        KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_NO,   KC_NO,
+        KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_PGDN, KC_END, _______,       _______, KC_PLUS, KC_MINS, KC_EQL, KC_LBRC, KC_RBRC, KC_BSLS,
+                                   _______, _______, MO(3),  _______,       _______, _______, _______, _______),
+
 
 [_LAYER3] = LAYOUT(
-        KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,                   KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
-        KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,                   KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
-        KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,                   KC_NO, KC_NO, RGB_TOG, RGB_HUI, RGB_SAI, RGB_VAI,
-        KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,        KC_NO, KC_NO, KC_NO, RGB_MOD, RGB_HUD, RGB_SAD, RGB_VAD,
-                        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS)
+        KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,                          KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
+        KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,                          KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
+        KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,                          KC_NO,   KC_NO,   RGB_TOG, RGB_HUI, RGB_SAI, RGB_VAI,
+        KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO, KC_NO,          KC_NO,   KC_NO,   KC_NO,   RGB_MOD, RGB_HUD, RGB_SAD, RGB_VAD,
+                                   _______, _______, _______, _______,      _______, _______, _______, _______)
 
 };
