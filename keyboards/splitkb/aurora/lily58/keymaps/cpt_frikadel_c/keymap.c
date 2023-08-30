5f32e12cfc54db850a0ae523498963cfed5e2483
Cpt. Frikadel <adkupper@gmail.com>
2023-08-30 22:50:08 +0200

    Keymap: swap control and super

--- keyboards/splitkb/aurora/lily58/keymaps/cpt_frikadel_c/keymap.c
+++ keyboards/splitkb/aurora/lily58/keymaps/cpt_frikadel_c/keymap.c
@@ -33,7 +33,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t* record){
          KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T,                  KC_Y, KC_U, KC_I, KC_O, KC_P, KC_MINS,
          KC_ESC, KC_A, KC_S, KC_D, KC_F, KC_G,                  KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT,
          KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_LBRC,    KC_RBRC, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RCTL,
-                        KC_LCTL, KC_LGUI, MO(1), KC_ENT,    KC_SPC, MO(2), KC_BSPC, KC_RALT),
+                        KC_LGUI, KC_LCTL, MO(1), KC_ENT,    KC_SPC, MO(2), KC_BSPC, KC_RALT),
 
 [_LAYER1] = LAYOUT(
         _______, _______, _______, _______, _______, _______,                        _______, _______, _______, _______, _______, KC_DEL,
